a729bfed86e89a27e4b867230213c136a6a49bf4
S. PAVAN KOUSHIK <surile211407.cs@gmail.com>
2025-06-11 11:53:37 +0530

    day 1
    Signed-off-by: S. PAVAN KOUSHIK <surile211407.cs@gmail.com>

---
+++ day1.js
@@ -0,0 +1,6 @@
+import readline from "readline";
+ const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+ rl.question("What's your name? ", name => {
+    console.log(`Hello, ${name}! Welcome to Zero2Stack.`);
+    rl.close();
+ });
