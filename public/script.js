263fdf1a7423620ec4b2f0568556e105e153a9c8
Suganyapragasam <psuganya281@gmail.com>
2025-06-13 08:06:16 +0400

    DOM exercise for day 2

---
+++ public/script.js
@@ -0,0 +1,4 @@
+ const btn = document.getElementById("btn");
+ const msg = document.getElementById("msg");
+ btn.addEventListener("click", () => {msg.innerText = "JS is fun! ��"; msg.classList.toggle("highlight");
+ });
