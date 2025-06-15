f4fde8ded85b8d6e3149a5b67180573fcf05869d
Sobika.R <149921366+SobikaSobi@users.noreply.github.com>
2025-06-15 20:13:20 +0530

    Create Day 4 server.js

---
+++ Day 4 server.js
@@ -0,0 +1,12 @@
+// server.js
+import express from "express";
+
+const app = express();
+
+app.get("/api/hello", (req, res) => 
+  res.json({ msg: "Hello from Express!" })
+);
+
+app.listen(3000, () => 
+  console.log("Listening on 3000")
+);
