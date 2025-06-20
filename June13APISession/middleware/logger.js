73afe425683955d0e16ad1f0eff54b3030ed6372
Suganyapragasam <158312527+Suganyapragasam@users.noreply.github.com>
2025-06-20 18:24:22 +0400

    Revert "June14 - API Session"

--- June13APISession/middleware/logger.js
+++
@@ -1,9 +0,0 @@
-// middleware/logger.js
-export const logger = (req, res, next) => {
-  const start = Date.now();
-  res.on('finish', () => {
-    const duration = Date.now() - start;
-    console.log(`${req.method} ${req.originalUrl} ${res.statusCode} - ${duration}ms`);
-  });
-  next();
-};
--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-# Pradeepa-zerostack
-To run this program we need to go inside the folder where is downloaded and run node followed by filename. 
-node hello.js
-Pre-requsisites: Ensure latest node js is installed
-
+# zero2stack
+Coding is fun when done right with the right folks
