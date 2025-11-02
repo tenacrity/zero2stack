cddd12226351179ea6622ccc1afbf93cd50200c8
Rohith Reddy <potharohithreddy@gmail.com>
2025-11-02 09:49:10 +0530

    Committied the Practice of Loops

--- Loops.js
+++ Loops.js
@@ -2,15 +2,24 @@ for(let i = 0; i < 10; i++) {
     console.log(i);
 }
 
+// for(let i = 0; i < 10; i++) {
+//     if(i % 2 === 0) {
+//         console.log("Even number: " + i);
+//     }
+//     else {
+//         console.log("Odd number: " + i);
+//     }
+// }
+
+//only even numbers
 for(let i = 0; i < 10; i++) {
     if(i % 2 === 0) {
         console.log("Even number: " + i);
     }
-    else {
-        console.log("Odd number: " + i);
-    }
 }
 
+
+//reverse loop
 for(let i = 10; i > 0; i--) {
     console.log(i);
 }
