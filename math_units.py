64821cd3fd662cabdb4738097db6140a685243d7
USHASWANI <singamushaswini002@gmail.com>
2025-11-12 17:32:52 +0530

    i update the pytest code

---
+++ math_units.py
@@ -0,0 +1,4 @@
+
+def is_even(number):
+    """Return True if number is even, else False"""
+    return number % 2 == 0
---
+++ test_math_units.py
@@ -0,0 +1,8 @@
+
+from  math_units import is_even
+
+def test_even_number():
+    assert is_even(4) == True
+
+def test_odd_number():
+    assert is_even(7) == False
